ccad8751a44929683df1b59f9329cd54f656ae91
rostik yuriev <rost44022552gmail.com>
2017-10-22 12:01:27 +0300

    changed title

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
  <head>
   <meta charset="utf-8">
-  <title>css4</title>
+  <title>Rostikx site</title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-dropdown/2.0.3/jquery.dropdown.css">
   <link href="style.css" rel="stylesheet">
